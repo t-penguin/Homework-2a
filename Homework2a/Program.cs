a2ddb12eb07fff212ebef931cc32b6e79b4beba4
Joseph <sourpina@gmail.com>
2025-02-12 19:22:14 -0500

    Initial assignment code

--- Homework2a/Program.cs
+++ Homework2a/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cards2;
+
+// loop while there's more input
+string input = Console.ReadLine();
+
+// Add your code between this comment
+// and the comment below. You can of
+// course add more space between the
+// comments as needed
+
+// declare a deck variables and create a deck object
+// DON'T SHUFFLE THE DECK
+
+
+// deal 2 cards each to 4 players (deal properly, dealing
+// the first card to each player before dealing the
+// second card to each player)
+
+
+// deal 1 more card to players 2 and 3
+
+
+// flip all the cards over
+
+
+// print the cards for player 1
+
+
+// print the cards for player 2
+
+
+// print the cards for player 3
+
+
+// print the cards for player 4
