65a0f065da3240bd1fb2c5e20442dccfac5800b3
Joseph <sourpina@gmail.com>
2025-02-12 19:32:50 -0500

    implemented card dealing section

--- Homework2a/Program.cs
+++ Homework2a/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Cards2;
 
 // loop while there's more input
-string input = Console.ReadLine();
+//string input = Console.ReadLine();
 
 // Add your code between this comment
 // and the comment below. You can of
@@ -10,15 +10,21 @@ string input = Console.ReadLine();
 
 // declare a deck variables and create a deck object
 // DON'T SHUFFLE THE DECK
-
+Deck deck = new Deck();
 
 // deal 2 cards each to 4 players (deal properly, dealing
 // the first card to each player before dealing the
 // second card to each player)
-
+List<Card>[] playerHands = new List<Card>[4];
+for (int i = 0; i < 2; i++)
+{
+    foreach (List<Card> playerHand in playerHands)
+        playerHand.Add(deck.TakeTopCard());
+}
 
 // deal 1 more card to players 2 and 3
-
+playerHands[1].Add(deck.TakeTopCard());
+playerHands[2].Add(deck.TakeTopCard());
 
 // flip all the cards over
 
